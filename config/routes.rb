9ec1174662a8044222579a57423c2ff5d0c2a634
frankaguirre <frankagos1502@gmail.com>
2024-06-06 09:16:36 -0500

    desarollo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pokemons/index'
+  get 'pokemons/show'
+  get 'pokemons/create'
+  get 'pokemons/update'
+  get 'pokemons/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
