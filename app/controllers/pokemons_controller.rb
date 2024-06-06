fcbd35501f16cc44779ff54682934306be86be40
frankaguirre <frankagos1502@gmail.com>
2024-06-06 09:28:49 -0500

    parte2

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,16 +1,39 @@
 class PokemonsController < ApplicationController
   def index
+    @pokemons = Pokemon.all 
+    render json: @pokemons
   end
 
   def show
+    @pokemon = Pokemon.find(params[:id])
+    render json: @pokemon
   end
 
   def create
+    @pokemon = Pokemon.create(
+      nombre: params[:nombre],
+      tipo: params[:tipo],
+      ps: params[:ps],
+      ataque: params[:ataque],
+      defensa: params[:defensa]
+    )
+    render json: @pokemon
   end
 
   def update
+    @pokemon = Pokemon.find(params[:id])
+    pokemon.update(
+      nombre: params[:nombre],
+      tipo: params[:tipo],
+      ps: params[:ps],
+      ataque: params[:ataque],
+      defensa: params[:defensa]
+    )
+    render json: @pokemon
   end
 
   def destroy
+    @pokemon = Pokemon.find(params[:id])
+    pokemon.destroy
   end
 end
